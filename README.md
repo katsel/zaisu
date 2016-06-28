93b87dd10dfc0cfdb8dc1d780a79daeb4b5dcb0e
katsel <katsel@users.noreply.github.com>
2016-06-28 16:07:27 +0200

    Start adding tests, update README accordingly

--- README.md
+++ README.md
@@ -8,7 +8,18 @@ written in Erlang.
 
 ## Running
 
+To generate the files needed to build a release, run
+
     $ make bootstrap-rel
+
+From now on, you can simply generate a new release and start the
+application with
+
     $ make run
+
+Tests can be run with
+
+    $ make tests
+
 
 tbc.
---
+++ src/tests/zaisu_tests.erl
@@ -0,0 +1,62 @@
+-module(zaisu_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+-define(APPS, [crypto, cowlib, ranch, cowboy, zaisu]).
+
+
+% Test descriptions
+
+start_stop_test_() ->
+    {
+        "The server can be started and stopped and is alive in between",
+        {
+            setup,
+            fun start/0, fun stop/1,
+            fun(_) ->
+                is_sup_alive()
+            end
+        }
+    }.
+
+
+% Setup functions
+
+start() ->
+    start_applications(?APPS).
+
+stop(_) ->
+    ok = stop_applications(?APPS).
+
+
+% Actual tests
+
+hello_test() ->
+    ok.
+
+is_sup_alive() ->
+    Pid = whereis(zaisu_sup),
+    ?_assert(erlang:is_process_alive(Pid)).
+
+
+% Helper functions
+
+start_applications(Apps) ->
+    start_applications(Apps, []).
+
+start_applications([], Acc) ->
+    lists:reverse(Acc);
+start_applications([App|Apps], Acc) ->
+    case application:start(App) of
+    {error, {already_started, _}} ->
+        start_applications(Apps, Acc);
+    {error, {not_started, Dep}} ->
+        start_applications([Dep, App | Apps], Acc);
+    {error, {not_running, Dep}} ->
+        start_applications([Dep, App | Apps], Acc);
+    ok ->
+        start_applications(Apps, [App|Acc])
+    end.
+
+stop_applications(Apps) ->
+    [application:stop(App) || App <- lists:reverse(Apps)],
+    ok.
